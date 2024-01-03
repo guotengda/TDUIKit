db545532e0e02b5cf520b0b94e19b95fbccf9955
Sherlock <Sherlock>
2024-01-03 20:03:56 +0800

    Initial Commit

---
+++ Tests/TDUIKitTests/TDUIKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import TDUIKit
+
+final class TDUIKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TDUIKit().text, "Hello, World!")
+    }
+}
